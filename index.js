8f75ddfb4679bb1a9516b71ee2c7feb804896223
Omar Gonzalez <ogonzaleznyc@gmail.com>
2021-04-24 14:08:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+  if (array.length === 1) {
+    return array[0]
+  }
+  if (array.length > 1) {
+    let i = 0;
+    let least; 
+    while (i <= array.length - 1) {
+      least = Math.min(array[i], array[i+1]);
+      i+=1;
+    }
+    return least
+  }
 }
 
-function selectionSort(array){
-
-}
+// function selectionSort(array){
+//   let finalArray = [];
+//   while (array.length > 0) {
+//     finalArray.push(findMinAndRemove(array))
+//   }
+//   return finalArray
+// }
